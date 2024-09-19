9a8139d0be2faff7e470fb183e2cbbb8ac3144f2
Anuj <anujgupta1532003@gmail.com>
2024-09-19 15:24:18 +0530

    all done

--- models/result.schema.js
+++ models/result.schema.js
@@ -1,25 +1,24 @@
 import mongoose from "mongoose";
 
-
 const resultSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true
+    username: {
+      type: String,
+      required: true,
     },
-    results:{
-        type: Array,
-        default: []
+    results: {
+      type: Array,
+      default: [],
     },
-    points:{
-        type: Number,
-        default: 0
+    points: {
+      type: Number,
+      default: 0,
     },
-    createdAt:{
-        type: Date,
-        default: new Date()
-    }
-});
-
-const Result = mongoose.model("Result", resultSchema);
-
-export default Result;
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+  });
+  
+  const Result = mongoose.model("Result", resultSchema);
+  
+  export default Result;
